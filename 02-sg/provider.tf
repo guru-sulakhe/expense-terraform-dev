6183eba9b8a741312d9e10d535acd5147dc2cf00
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-09-03 19:39:02 +0530

    apps

--- 02-sg/provider.tf
+++ 02-sg/provider.tf
@@ -7,9 +7,9 @@ terraform {
   }
   backend "s3" {
     bucket = "guru97s-remote-state"
-    key    = "expense-terraform-dev-sg"
+    key    = "guru97s-locking-expense-terraform-dev-sg"
     region = "us-east-1"
-    dynamodb_table = "guru97s-locking-dynamodb"
+    dynamodb_table = "guru97s-locking-expense-terraform-dev"
   }
 }
 # by including backend s3 terraform will create .state file in s3-bucket of AWS
